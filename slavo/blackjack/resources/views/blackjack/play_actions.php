c19b0ca9c4a1d0625b71e3a21306a905bdca2c80
Unknown <linda.damario@outlook.com>
2017-06-02 10:18:04 +0200

    changes

---
+++ slavo/blackjack/resources/views/blackjack/play_actions.php
@@ -0,0 +1,9 @@
+<a href="http://<?php echo $url?>?route=hit&id=<?php echo $game_id ?>">Hit</a>
+<a href="http://<?php echo $url?>?route=stand&id=<?php echo $game_id ?>">Stand</a>
+
+<script src="../jquery-3.2.1.js"></script>
+<script>
+
+    
+
+</script>
